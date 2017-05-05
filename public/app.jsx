5a7eb204643d4cb4b046e3c4d64d06bcda709710
mpedersen15 <mpedersen@mindrocketdesign.com>
2017-05-05 12:53:17 -0600

    refactored components, updated webpack config

--- public/app.jsx
+++ public/app.jsx
@@ -1,69 +1,8 @@
-var GreeterMessage = React.createClass({
-	render: function(){
-		var name = this.props.name;
-		var message = this.props.message;
-		return (
-			<div>
-				<h1>Hello {name}!</h1>
-				<p>{message}</p>
-			</div>
-		);
-	}
-});
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Greeter = require('Greeter');
 
-var GreeterForm = React.createClass({
-	onFormSubmit: function(e){
-		e.preventDefault();
-		
-		var name = this.refs.name.value;
-		
-		if (name.length > 0){
-			this.refs.name.value = "";
-			
-			this.props.onNewName(name);
-			
-		}
-	},
-	render: function(){
-		return (
-			<form onSubmit={this.onFormSubmit}>
-				<input type="text" ref="name" />
-				<button>Set Name</button>
-			</form>
-		);
-	}
-});
-
-var Greeter = React.createClass({
-	getDefaultProps: function(){
-		return {
-			name: 'React',
-			message: 'This is from a component.'
-			}
-	},
-	getInitialState: function(){
-		return {
-			name: this.props.name
-		}
-	},
-	handleNewName: function(name){
-		this.setState({
-			name: name
-		});	
-	},
-	render: function(){
-		var name = this.state.name;
-		var message = this.props.message;
-		return (
-			<div>
-				<GreeterMessage name={name} message={message}/>
-				<GreeterForm onNewName={this.handleNewName}/>
-			</div>
-		);
-	}
-});
-
-var firstName = "Matt";
+var firstName = "Jen";
 ReactDOM.render(
 	<Greeter name={firstName} message="An alternative message"/>,
 	document.getElementById('app')
