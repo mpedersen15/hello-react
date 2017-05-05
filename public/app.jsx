db9b308c0c8950268c3fb7e8ef96576580ac7a2e
Matt Pedersen <mpedersen@mindrocketdesign.com>
2017-05-04 18:13:30 -0600

    finished first test component

--- public/app.jsx
+++ public/app.jsx
@@ -1,8 +1,15 @@
 var Greeter = React.createClass({
-	
+	render: function(){
+		return (
+			<div>
+				<h1>Hello React!</h1>
+				<p>It's a whole new world!</p>
+			</div>
+		);
+	}
 });
 
 ReactDOM.render(
-	<h1>Hello React!</h1>,
+	<Greeter/>,
 	document.getElementById('app')
 );
